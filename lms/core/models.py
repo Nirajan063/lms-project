215d8bb9206e77b3d2c1f9b123daf50f7440ec4b
Nirajan063 <nirajansingh632@gmail.com>
2025-06-15 14:38:30 +0545

    created admin pannel,  teacher and student pannel also

--- lms/core/models.py
+++ lms/core/models.py
@@ -46,5 +46,18 @@ class Teacher(models.Model):
     def __str__(self):
         return self.full_name
     
-
-    
+    class Assignment(models.Model):
+        title= models.CharField(max_length=100,null=False,blank=False,verbose_name='Assignmenyt Title')
+        start_date= models.DateField(default='Start date', blank=False, null=False,verbose_name='start date')
+        end_date= models.DateField(default='End Date', blank=False, null=False,verbose_name='End Date')
+        question_file= models.models.FileField(_ upload_to='assignments/questions/', null= True,blank= True,verbose_name='Assignment question files')
+        question= models.TextField(null=True,blank=True, verbose_name='assignment questions')
+        remark= models.CharField(max_length=100, null=False,blank=False,verbose_name='assignment Details')
+        full_mark = models.FloatField(blank=False, null=False)
+        teacher =models.ForeignKey(Teacher,on_delete=models.CASCADE,verbose_name='uploaded by' )
+        class meta:
+            verbose_name ='Assignment '
+            verbose_name_plural= 'Assignment'
+            ordering= ['due_Date']
+        def __str__(self):
+            return self.title
