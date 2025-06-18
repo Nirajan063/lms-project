39cb718d9e92eb28e44a6d0e4e599895e4f592b4
Nirajan063 <nirajansingh632@gmail.com>
2025-06-18 15:02:49 +0545

    Django project lms: font awesome, google font, static css, Teacher EDIT, Update & delete

--- lms/core/urls.py
+++ lms/core/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from core.views import HomePageView,TeacherCreateView, TeacherListView
+from core.views import (HomePageView,
+                        TeacherCreateView, 
+                        TeacherListView,
+                        TeacherUpdateView,
+                        TeacherDeleteView ,
+                        TeacherDetailView)
 
 urlpatterns = [
     #path('', views.home, name='home') -function base views loading
     path('', HomePageView.as_view(), name='home'),
     path('teacher/list/', TeacherListView.as_view(), name='teacher.index'),
     path('teacher/create/', TeacherCreateView.as_view(), name='teacher.create'),
+    path('teacher/edit/<int:pk>/', TeacherUpdateView.as_view(), name='teacher.edit'),
+    path('teacher/delete/<int:pk>/', TeacherDeleteView.as_view(), name='teacher.delete'),
+    path('teacher/detail/<int:pk>/', TeacherDetailView.as_view(), name='teacher.detail'),
 ]
--- lms/core/views.py
+++ lms/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView,DetailView
 from core.models import Teacher, Student, Assignment
+from django.urls import reverse_lazy
 
 # Create your views here.
 class HomePageView(TemplateView):
@@ -13,8 +14,27 @@ class TeacherListView(ListView):
     context_object_name = 'teachers'
     paginate_by = 5 
 
-class TeacherCreateView(TemplateView):
+class TeacherCreateView(CreateView):
     model= Teacher
     fields=['full_name', 'email', 'department', 'phone_no', 'user']
     template_name = 'teachers/teacher_form.html'
-    success_url = 'teacher.index'
+    success_url = reverse_lazy('teacher.index')
+
+
+class TeacherUpdateView(UpdateView):
+    model = Teacher
+    fields = ['full_name', 'email', 'department', 'phone_no']
+    template_name = 'teachers/teacher_form.html'
+    success_url = reverse_lazy('teacher.index')
+
+class TeacherDetailView(DetailView):
+    model= Teacher
+    template_name= 'teachers/teacher_detail.html'
+    context_object_name = 'teacher'
+
+
+class TeacherDeleteView(DeleteView):
+    model = Teacher
+    template_name = 'teachers/teacher_confirm_delete.html'
+    context_object_name = 'teacher'
+    success_url = reverse_lazy('teacher.index')
